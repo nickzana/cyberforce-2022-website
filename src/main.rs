a558bbba707bb9280184a5ef56d3c20de9a9bba4
Nick Zana <me@nickzana.dev>
2022-11-05 12:58:15 -0400

    change listen port

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() {
         .route("/manufacturing", get(manufacturing));
 
     // run it
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
